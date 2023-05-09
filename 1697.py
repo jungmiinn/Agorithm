f852ea2f9e09501691cc1a716a019420b4823b3e
jungmiinn <pjm9562@naver.com>
2023-05-09 17:29:45 +0900

    0509

--- 1697.py
+++ 1697.py
@@ -1,20 +1,48 @@
 import sys
-
-ip = input.split(' ')
-n, k = int(ip[0]), int(ip[1])
-
-def compare(point):
-    dis1 = abs(k-(point-1))
-    dis2 = abs(k-(point+1))
-    dis3 = abs(k-(2*point))
-    res = min(dis1, dis2, dis3)
-    return res
+import math
 
 q = []
+ip = input().split(' ')
+n, k = int(ip[0]), int(ip[1])
+q.append(n)
 
+pos = [0 for i in range(100001)] # 방문처리
+pos[n] = 1
 front = 0
-rear = 0
 now = n
-distance = 0
+sec = 0
+cnt = 0
 
-while(1):
+def queue(point):
+    global front
+    global cnt
+    global sec
+
+    new1 = point - 1
+    if pos[new1] == 0:
+        pos[new1] = 1
+        q.append(new1)
+
+    new2 = point + 1
+    if pos[new2] == 0:
+        pos[new2] = 1
+        q.append(new2)
+
+    new3 = 2*point
+    if pos[new3] == 0:
+        pos[new3] = 1
+        q.append(new3)
+    cnt = cnt + 3
+
+
+while True:
+    now = q[front]
+    if k == now:
+        # print(math.log(2*cnt+1, 3))
+        sec = int(math.floor(math.log(2*cnt+1, 3)))
+        print(sec)
+        break
+    else:
+        front = front + 1
+        queue(now)
+
