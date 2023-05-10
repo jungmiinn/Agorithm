1e9fef0077d50d5cc72d83700e5c4b3615a0fa24
jungmiinn <76022058+jungmiinn@users.noreply.github.com>
2023-05-10 13:51:01 +0900

    0510

--- 1697.py
+++ 1697.py
@@ -4,6 +4,14 @@ import math
 q = []
 ip = input().split(' ')
 n, k = int(ip[0]), int(ip[1])
+
+if n > 100000 or n < 0:
+    exit
+if k > 100000 or k < 0:
+    exit
+        
+    
+
 q.append(n)
 
 pos = [0 for i in range(100001)] # 방문처리
@@ -19,21 +27,25 @@ def queue(point):
     global sec
 
     new1 = point - 1
-    if pos[new1] == 0:
+    if new1 >=0 and pos[new1] == 0:
         pos[new1] = 1
         q.append(new1)
 
     new2 = point + 1
-    if pos[new2] == 0:
+    if new2 <= 100_000 and pos[new2] == 0:
         pos[new2] = 1
         q.append(new2)
 
     new3 = 2*point
-    if pos[new3] == 0:
+    if new3 <= 100_000 and pos[new3] == 0:
         pos[new3] = 1
         q.append(new3)
     cnt = cnt + 3
 
+
+
+
+    
 
 while True:
     now = q[front]
