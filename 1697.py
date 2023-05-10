1fa99903b35e19e7f1baa9a895c788f46edd208b
jungmiinn <76022058+jungmiinn@users.noreply.github.com>
2023-05-10 16:51:26 +0900

    Update 1697.py

--- 1697.py
+++ 1697.py
@@ -1,18 +1,10 @@
 import sys
 import math
 
-q = []
+q = list()
 ip = input().split(' ')
 n, k = int(ip[0]), int(ip[1])
-
-if n > 100000 or n < 0:
-    exit
-if k > 100000 or k < 0:
-    exit
-        
-    
-
-q.append(n)
+q.append([n, 0])
 
 pos = [0 for i in range(100001)] # 방문처리
 pos[n] = 1
@@ -21,38 +13,39 @@ now = n
 sec = 0
 cnt = 0
 
+
 def queue(point):
     global front
     global cnt
-    global sec
+    cnt = cnt + 3
+    sec = int(math.floor(math.log(2*cnt+1, 3)))
+    level = sec
 
     new1 = point - 1
     if new1 >=0 and pos[new1] == 0:
         pos[new1] = 1
-        q.append(new1)
+        q.append([new1, level])
 
     new2 = point + 1
     if new2 <= 100_000 and pos[new2] == 0:
         pos[new2] = 1
-        q.append(new2)
+        q.append([new2, level])
 
     new3 = 2*point
     if new3 <= 100_000 and pos[new3] == 0:
         pos[new3] = 1
-        q.append(new3)
-    cnt = cnt + 3
+        q.append([new3, level])
+
+        
 
 
 
-
-    
 
 while True:
-    now = q[front]
+    now = q[front][0]
+    nowlevel = q[front][1]
     if k == now:
-        # print(math.log(2*cnt+1, 3))
-        sec = int(math.floor(math.log(2*cnt+1, 3)))
-        print(sec)
+        print(nowlevel)
         break
     else:
         front = front + 1
