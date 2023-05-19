207bafac3a9782a86c6819a23667f6156b087bfc
jungmiinn <76022058+jungmiinn@users.noreply.github.com>
2023-05-19 23:29:49 +0900

    0519

--- 1254.py
+++ 1254.py
@@ -5,5 +5,12 @@ copied_ary = origin_ary
 front = 0
 rear = len(origin_ary) - 1
 
-def compare():
-    res
+def judge(ary):
+    ori_ary = ary
+    copy_ary = ary
+    start = 0
+    finish = 0
+    for i in range(len(ary)):
+        if ori_ary[i] == copy_ary[-i-1]:
+            start = i
+        
