ac7eccefab76fd90765c2b08ebf1d774f9503537
jungmiinn <pjm9562@naver.com>
2023-05-24 17:41:11 +0900

    0524new21

--- 1254.py
+++ 1254.py
@@ -3,28 +3,39 @@ import sys
 
 origin_ary = input().split('')
 copied_ary = origin_ary
+blen = int(len(origin_ary))
 front = 0
 rear = len(origin_ary) - 1
-start = -1
+start = 0
 finish = 0
 
 def judge(ary):
     ori_ary = ary
     copy_ary = ary.reverse
+    idx = 0
     global start
     global finish
-    for i in range(len(ary)):
-        if ori_ary[i] == copy_ary[i]:
-            if start == -1:
-                start = i
-            else:
-                finish = i
+    for i in range(blen): # copy배열을위한 반복문
+        for j in range(blen): # 비교하는 반복문
+           if ori_ary[j] == copy_ary[idx]:
+               start = j
+               i = start
+               break
+        idx = idx + 1
+
+
     res = -1  # 0은 이미 완성 1은 부분완성 2는 그냥 없음
     if start == 0 & finish == len(ary)-1: # 완성
         res = 0
     elif start > 0 & finish == len(ary)-1: # 부분완성
         res = 1
-    elif start == -1:
+    elif start == -1: # 그냥없음
         res = 2
 
     return res
+
+n = judge(origin_ary)
+if n == 0 :
+
+elif n == 1:
+elif n ==2:
