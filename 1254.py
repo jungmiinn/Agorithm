12fa3cebf03bfa014eb4fe242736b7f14918ef6d
jungmiinn <76022058+jungmiinn@users.noreply.github.com>
2023-05-24 17:16:55 +0900

    0524

--- 1254.py
+++ 1254.py
@@ -4,13 +4,26 @@ origin_ary = input().split('')
 copied_ary = origin_ary
 front = 0
 rear = len(origin_ary) - 1
+start = -1
+finish = 0
 
 def judge(ary):
     ori_ary = ary
-    copy_ary = ary
-    start = 0
-    finish = 0
+    copy_ary = ary.reverse
+    global start
+    global finish
     for i in range(len(ary)):
-        if ori_ary[i] == copy_ary[-i-1]:
-            start = i
-        
+        if ori_ary[i] == copy_ary[i]:
+            if start == -1:
+                start = i
+            else:
+                finish = i
+    res = -1  # 0은 이미 완성 1은 부분완성 2는 그냥 없음
+    if start == 0 & finish == len(ary)-1: # 완성
+        res = 0
+    elif start > 0 & finish == len(ary)-1: # 부분완성
+        res = 1
+    elif start == -1:
+        res = 2
+
+    return res
