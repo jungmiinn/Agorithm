15ed85a158e99ba4e5dc11d8c9d5108a707f46a3
jungmiinn <76022058+jungmiinn@users.noreply.github.com>
2023-05-25 21:31:39 +0900

    Update 1254.py

--- 1254.py
+++ 1254.py
@@ -1,41 +1,41 @@
 import sys
 
 
-origin_ary = input().split('')
-copied_ary = origin_ary
+a = input()
+origin_ary = list(a)
 blen = int(len(origin_ary))
-front = 0
-rear = len(origin_ary) - 1
-start = 0
-finish = 0
+start = -1
 
 def judge(ary):
     ori_ary = ary
-    copy_ary = ary.reverse
-    idx = 0
+    copy_ary = ary[::-1]
+    idx = -1
     global start
-    global finish
-    for i in range(blen): # copy배열을위한 반복문
-        for j in range(blen): # 비교하는 반복문
-           if ori_ary[j] == copy_ary[idx]:
-               start = j
-               i = start
-               break
-        idx = idx + 1
-
-
-    res = -1  # 0은 이미 완성 1은 부분완성 2는 그냥 없음
-    if start == 0 & finish == len(ary)-1: # 완성
-        res = 0
-    elif start > 0 & finish == len(ary)-1: # 부분완성
-        res = 1
-    elif start == -1: # 그냥없음
-        res = 2
-
-    return res
-
+    global blen
+    for i in range(blen): # start 지정
+        if ori_ary[i] == copy_ary[0]:
+            start = i
+            for j in range(blen-i):
+                if ori_ary[j+i] != copy_ary[j]:
+                    start = -1
+                    break
+            if start != -1:
+                break
+                       
+                   
+    if start == blen -1:
+        return 2
+    elif start == 0:
+        return 0
+    else:
+        return 1
+    
+ 
 n = judge(origin_ary)
-if n == 0 :
 
-elif n == 1:
-elif n ==2:
+if n == 0 : # 이미완성형
+    print(blen)
+elif n == 1: # 부분완성형
+    print(blen + start)
+elif n ==2: # 걍 없음
+    print(blen * 2 -1)
