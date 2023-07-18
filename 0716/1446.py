5fca2b9839573c9b0e90c6223a337e8df3a86d2a
jungmiinn <pjm9562@naver.com>
2023-07-18 16:00:49 +0900

    nothing

--- 0716/1446.py
+++ 0716/1446.py
@@ -1,32 +1,21 @@
 # 1446 세준이의 등교 지름길
 import heapq
-import builtins
+import sys
 
 a = list(map(int, input().split(' ')))
 n = a[0]
 d = a[1]
-heap = list()
-accul = 0
+arr = list()
+left_d = d
 
 for i in range(n):
     temp = list(map(int, input().split(' ')))
-    if temp[1] <= d:
-        heapq.heappush(heap, (temp[2], temp))
+    if temp[1] <= d and temp[2] < temp[1]-temp[0]:
+        # print(pri)
+        heapq.heappush(arr,(temp[0], tem
+                            p))
 
-
-start = [0, 0, 0]
-
+dis = list()
 for i in range(n):
-    print(start)
-    if start[1] == d:
-        break
-    else:
-        temp = heapq.heappop(heap)[1]
 
-    if temp[1]-temp[0] < temp[3]:
-        accul = accul + temp[1]-temp[0]
-    else:
-        accul += temp[2]
-    start = temp
-
-print(accul)
+    
