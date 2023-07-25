a51f20478cbf386987006bb648e9dc43af95d28e
jungmiinn <76022058+jungmiinn@users.noreply.github.com>
2023-07-25 11:42:57 +0900

    Update 1446.py
    왜 틀렸는지 모르겠음

--- 0716/1446.py
+++ 0716/1446.py
@@ -5,21 +5,25 @@ import sys
 a = list(map(int, input().split(' ')))
 n = a[0]
 d = a[1]
-path = [1 for i in range(d)]
-print(path)
+path = [i for i in range(d+1)]
 
 arr = list()
 now_d = 0
 
 for i in range(n):
     temp = list(map(int, input().split(' ')))
-    if temp[1] <= d and temp[2] < temp[1]-temp[0]:
+    if temp[1] <= d and temp[2] < temp[1]-temp[0]:  # 말도 안되는거 걸러주기
         heapq.heappush(arr, (temp[0], temp))
 
+for i in range(len(arr)):
+    new = heapq.heappop(arr)[1]  # new = [0, 50, 10]
+    start = new[0]
+    des = new[1]
+    cost = new[2]
+    if path[des] > cost + path[start]:
+        path[des] = cost + path[start]
+        for j in range(des+1, d+1):
+            path[j] = path[j-1] + 1
+            print(path[j])
 
-def gototheSC(start):
-    if now_d == d:
-        return
-    sc = heapq.heappop(arr)
-    for i in range(d):
-        left_d -= path[i]
+print(path[d])
