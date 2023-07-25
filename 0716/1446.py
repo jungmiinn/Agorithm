01762917cefb3e1d34a8f5aca17a7c9cde5ed39e
jungmiinn <76022058+jungmiinn@users.noreply.github.com>
2023-07-25 13:50:47 +0900

    Update 1446.py
    sibal

--- 0716/1446.py
+++ 0716/1446.py
@@ -12,11 +12,12 @@ now_d = 0
 
 for i in range(n):
     temp = list(map(int, input().split(' ')))
-    if temp[1] <= d and temp[2] < temp[1]-temp[0]:  # 말도 안되는거 걸러주기
+    if temp[1] <= d:  # 목적지 보다 뒤에 있는거는 걍 안넣음
         heapq.heappush(arr, (temp[0], temp))
 
 for i in range(len(arr)):
     new = heapq.heappop(arr)[1]  # new = [0, 50, 10]
+    # print(new)
     start = new[0]
     des = new[1]
     cost = new[2]
@@ -24,5 +25,7 @@ for i in range(len(arr)):
         path[des] = cost + path[start]
         for j in range(des+1, d+1):
             path[j] = path[j-1] + 1
+            print("j, cost: ", j, path[j])
+    print("")
 
 print(path[d])
