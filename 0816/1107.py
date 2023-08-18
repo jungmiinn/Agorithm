049234ceff975d46397b863a81c8f81fe1e31672
jungmiinn <76022058+jungmiinn@users.noreply.github.com>
2023-08-18 16:30:00 +0900

    리모컨 솔

--- 0816/1107.py
+++ 0816/1107.py
@@ -1,24 +1,21 @@
 goal = int(input())
 n = int(input())
 
+if n:
+    errlist = set(input().split()) # 안되는거 저장
+    # board = set(set(board) - set(errlist)) # 되는거 저장
+else:
+    errlist = set()
+
 gap = abs(goal - 100)
-arr = list(map(int, str(goal))) # goal 따로 떼서 저장
 
-board = [i for i in range(10)]
-errlist = list(map(int, input().split(' '))) # 안되는거 저장
-board = list(set(board) - set(errlist)) # 되는거 저장
-print(board)
+for i in range(1000001):
+    for num in str(i):
+        if num in errlist:
+            break
+    else:
+        gap = min(gap, len(str(i))+abs(i - goal))
 
-l = len(str(goal))
+print(gap)
 
-
-for i in range(l):
-    if arr[i] not in board:
-        arr[i] = -1
-
-if gap < l: # +, - 로만 해결 가능
-    print(gap)
-elif -1 not in arr: # 못치는거 없을때는 그냥 쌩으로 쳐 
-    print(l)
-else: # 본격적인 문제상황
 
